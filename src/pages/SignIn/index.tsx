d259ee7cbd6250e5c48cf2de3a60ec3e60b5dc5b
Felipe A Pellegrini <felipeapellegrini@gmail.com>
2020-07-09 03:00:29 -0300

    Iniciando o aplicativo mobile configurando a tela de login e criando componentes de input e button

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import Button from '../../components/Button'
+import Input from '../../components/Input';
+import logoImg from '../../assets/logo.png';
+import Icon from 'react-native-vector-icons/Feather';
+import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles';
+
+
+const SignIn: React.FC = () => {
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView>
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button onPress={() => (console.log('Deu'))}>Entrar</Button>
+
+            <ForgotPassword onPress={() => { console.log('Esqueceu') }}>
+              <ForgotPasswordText>
+                Esqueci minha senha
+        </ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <CreateAccountButton onPress={() => { console.log('Criou conta') }}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>
+          Criar conta
+        </CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
+  );
+}
+
+export default SignIn;
